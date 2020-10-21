e04a8d7181a0a7b64511b2add50ec4efed0b58b1
ppopow <pawel.popow@gmail.com>
2020-10-21 14:00:59 +0200

    nowe zadanie 36

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function sprawdzLiczbe(liczba1, liczba2) {
+   
+    if ((liczba1 == 20) || (liczba2 == 20) || (liczba1 + liczba2 <= 20)) {
+        return true;
+
+    }
+}
+
+ console.log(sprawdzLiczbe(2, 12));
+
+ console.log(sprawdzLiczbe(12, 12));
+
+
